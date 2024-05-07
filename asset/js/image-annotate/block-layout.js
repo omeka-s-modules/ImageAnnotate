966a6c275f199ceef32f6be7448462bd7cc5a9f5
Jim Safley <jimsafley@gmail.com>
2024-05-07 18:18:39 -0400

    Continue dev on image annotate block layout

--- asset/js/image-annotate/block-layout.js
+++ asset/js/image-annotate/block-layout.js
@@ -1,43 +1,43 @@
 $(document).ready(function() {
 
-// @todo: Handle initial "Apply changes"
+// Handle
 $('#attachment-confirm-panel button').on('click', function(e) {
     const block = $('.selecting-attachment').closest('.block');
     if ('imageAnnotate' !== block.data('blockLayout')) {
         return; // This is not an imageAnnotate block. Do nothing.
     }
     const containerWrapper = block.find('.image-annotate-container-wrapper');
+    const container = block.find('.image-annotate-container');
+    const image = block.find('img.image-annotate-image');
     const mediaId = parseInt(block.find('input.media').val(), 10);
     const mediaIdCurrent = parseInt(containerWrapper.data('mediaIdCurrent'), 10);
     const apiEndpointUrl = containerWrapper.data('apiEndpointUrl');
+    const anno = container.data('anno');
+    if (!mediaId) {
+        // This item has no media.
+        image.off('load');
+        image.attr('src', '');
+        containerWrapper.data('mediaIdCurrent', '');
+        return;
+    }
     if (mediaId === mediaIdCurrent) {
-        return; // This is the same media. Do nothing.
+        // This is the same media. Do nothing.
+        return;
     }
     // Get the large thumbnail URL from the API.
     $.get(`${apiEndpointUrl}/media/${mediaId}`, function(data) {
-        const container = block.find('.image-annotate-container');
-        const image = block.find('img.image-annotate-image');
-        // Destroy the current annotation interface and initialize a new one.
+        // First, remove all load event handlers to prevent triggering multiple.
+        // Then, on image load, destroy the current annotation interface and
+        // initialize a new one.
         image.off('load');
         image.on('load', function() {
-            const anno = container.data('anno');
-            anno.destroy();
+            if (anno) anno.destroy();
             ImageAnnotate.initEdit(container, []);
-            containerWrapper.data('mediaIdCurrent', mediaId);
         });
-        image.attr('src', data['o:thumbnail_urls']['large']);
+        // Load the new image.
+        image.attr('src', data['thumbnail_display_urls']['large']);
+        containerWrapper.data('mediaIdCurrent', mediaId);
     });
 });
-
-// @todo: Handle "Delete attachment"
-$('#blocks').on('click', '.delete', function(e) {
-    const thisButton = $(this);
-    const block = thisButton.closest('.block');
-    if ('imageAnnotate' !== block.data('blockLayout')) {
-        return;
-    }
-});
-
-// @todo: Handle "Open attachment options" > "Apply changes" i.e. change item/media
 
 });
