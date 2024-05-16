3fe1d02a78efd8913afe2ba78259f1ddca35f1f8
Jim Safley <jimsafley@gmail.com>
2024-05-16 15:15:51 -0400

    Minor JS refactor

--- asset/js/image-annotate.js
+++ asset/js/image-annotate.js
@@ -2,53 +2,35 @@ const ImageAnnotate = {
 
     /**
      * Initialize a show annotations container.
-     *
-     * @param {jQuery object} container
-     * @param {array} annotations
-     * @param {string} imageSrc
      */
     initShow: function(container, annotations, imageSrc) {
-        const getAnno = function(image) {
-            return Annotorious.init({
-                image: image[0],
-                // Display annotations in read-only mode.
-                readOnly: true,
-                // Remove TAG widget by setting only COMMENT.
-                widgets: ['COMMENT'],
-            });
+        const annoConfig = {
+            // Display annotations in read-only mode.
+            readOnly: true
         };
-        ImageAnnotate.init(container, annotations, imageSrc, getAnno);
+        ImageAnnotate.init(container, annotations, imageSrc, annoConfig);
     },
 
     /**
      * Initialize an edit annotations container.
-     *
-     * @param {jQuery object} container
-     * @param {array} annotations
-     * @param {string} imageSrc
      */
     initEdit: function(container, annotations, imageSrc) {
-        const getAnno = function(image) {
-            return Annotorious.init({
-                image: image[0],
-                // Use percent in the event the image dimensions change.
-                fragmentUnit: 'percent',
-                // Remove TAG widget by setting only COMMENT.
-                widgets: ['COMMENT'],
-            });
+        const annoConfig = {
+            // Use percent in the event the image dimensions change.
+            fragmentUnit: 'percent'
         };
-        ImageAnnotate.init(container, annotations, imageSrc, getAnno);
+        ImageAnnotate.init(container, annotations, imageSrc, annoConfig);
     },
 
     /**
      * Initialize an annotations container.
      *
-     * @param {jQuery object} container An annoations container
+     * @param {object} container A jQuery annoations container
      * @param {array} annotations An array of annotations in W3C Web Annotation format
      * @param {string} imageSrc A URL to the image
-     * @param {function} getAnno A function that returns the Annotorious object
+     * @param {object} annoConfig An Annotorious configuration object
      */
-    init: function(container, annotations, imageSrc, getAnno) {
+    init: function(container, annotations, imageSrc, annoConfig) {
         const image = container.find('.image-annotate-image');
         // On image load, destroy the current annotation interface, if any, and
         // initialize a new one. Note the use of one() to ensure the handler is
@@ -56,7 +38,11 @@ const ImageAnnotate = {
         image.one('load', function() {
             let anno = container.data('anno');
             if (anno) anno.destroy();
-            anno = getAnno(image);
+            // Set the image DOM element.
+            annoConfig.image = image[0];
+            // Remove TAG widget by setting only COMMENT.
+            annoConfig.widgets = ['COMMENT'];
+            anno = Annotorious.init(annoConfig);
             anno.setAnnotations(annotations);
             container.data('anno', anno);
         });
