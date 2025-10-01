532d78832d120c2321f664fb6e61338bd3bd7cde
Kim Nguyen <design@kimisgold.com>
2025-10-01 10:03:26 -0400

    Assign $imageAlt outside of conditional.
    (#7)

--- src/Site/BlockLayout/ImageAnnotateMedia.php
+++ src/Site/BlockLayout/ImageAnnotateMedia.php
@@ -96,6 +96,7 @@ class ImageAnnotateMedia extends AbstractBlockLayout implements TemplateableBloc
         $item = null;
         $media = null;
         $imageSrc = null;
+        $imageAlt = null;
         $caption = null;
         if ($attachments && $media = $attachments[0]->media()) {
             $item = $media->item();
